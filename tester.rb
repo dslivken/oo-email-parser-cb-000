f00ab2cf5023a6817c8ac297bcda70a8e2716610
David Slivken <slivken@gmail.com>
2018-07-01 01:00:39 +0000

    automatically backed up by learn

---
+++ tester.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class EmailParser
+
+  attr_accessor :list, :emails
+
+  def initialize (emails)
+    @emails=emails
+  end
+
+  def parse
+    temp=[]
+    @emails.split(',').each do |e|
+      if !temp.include(e)
+        temp<<e
+      end
+    end
+    temp
+  end
+end
+
+emails = "john@doe.com, person@somewhere.org"
+
+binding pry
